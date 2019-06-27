4a55f7bdb45f03b418b86330566c47c580556d30
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-06-27 18:32:12 -0400

    update config file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 720,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -13,23 +20,29 @@ var game = new Phaser.Game(config);
 
 function preload ()
 { 
-    this.load.image('backgrd','../asset/maps/1-1.png')
-    this.load.spritesheet('frog-mario','../asset/sprites/mario-frog-layer-1.png',{frameWidth: 32, frameHeight: 48})
-    this.load.spritesheet('left-mario','../asset/spites/mario-frog-walk-right.png',{frameWidth: 32, frameHeight: 48})
+    this.load.image('bckgrd',"#666666")
+    this.load.image('platform1','../asset/maps/map1-1/map-1-platform-part-1.png')
+    this.load.image('platform2','../asset/maps/map1-1/map-1-platform-part-2.png')
+    this.load.image('platform3','../asset/maps/map1-1/map-1-platform-part-3.png')
+    this.load.image('platform4','../asset/maps/map1-1/map-1-platform-part-4.png')
+    this.load.image('platform5','../asset/maps/map1-1/map-1-platform-part-5.png')
+    this.load.image('platform6','../asset/maps/map1-1/map-1-platform-part-6.png')
+    this.load.spritesheet('frog-mario','../asset/sprites/mario-frog-layer-1.png',{frameWidth: 35, frameHeight: 44})
+    this.load.spritesheet('left-mario','../asset/sprites/mario-frog-walk-right.png',{frameWidth: 32, frameHeight: 48})
 }
 
 function create ()
 {
-    this.add.image(2850, 360,'backgrd')
+    platforms = this.physics.add.staticGroup()
+    platforms.create(600, 480, 'platform1');
+
+
+    player = this.physics.add.sprite(300, 250, 'frog-mario').setDisplaySize(100, 100);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 }
 
-function update ()
-{
+function update (){
+    
 }
 
-//Listens for when the game size changes
-window.addEventListener('resize', function (event) {
-
-    game.scale.setMaxZoom();
-
-}, false);
